94c667dda5545017868491ff1eea840045ccbd43
Jeremy Barnes <jeremyb1988@gmail.com>
2020-03-28 10:12:59 -0700

    refactored theme-ui and their components.

--- src/components/Cart/Buttons/RemoveProduct.js
+++ src/components/Cart/Buttons/RemoveProduct.js
@@ -1,5 +1,5 @@
 /** @jsx jsx */
-import { jsx, Styled } from "theme-ui";
+import { jsx, Styled, Text } from "theme-ui";
 import React from "react";
 
 import { removeCartItem } from "../HelperFns/index";
@@ -10,25 +10,14 @@ var { useContext } = React;
 const RemoveProduct = props => {
 	//  get access to all of my current items in my cart through the store.
 	var [store, setStore] = useContext(StoreContext);
-
 	return (
-		<Styled.h5
+		<Text
 			onClick={() => removeCartItem(store, setStore, props)}
-			sx={{
-				textTransform: "uppercase",
-				color: "grays.4",
-				cursor: "pointer",
-				width: "15%",
-				letterSpacing: props => `${props.letterSpacing[2]}em`,
-				transition: props => `${props.transitions[0]}s`,
-				"&:hover": {
-					transition: props => `${props.transitions[0]}s`,
-					color: "primary"
-				}
-			}}
+			variant={"interactive.toPrimary"}
+			sx={{ width: "25%" }}
 		>
 			Remove
-		</Styled.h5>
+		</Text>
 	);
 };
 
--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -1,15 +1,9 @@
+import { type } from "./type";
+import { components } from "./components";
+
 const theme = {
-	fonts: {
-		body:
-			'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", sans-serif'
-	},
-	space: [0, 15, 30, 45, 60, 90, 120],
-	lineHeights: [1, 1.25, 1.5],
-	letterSpacing: [-0.1, 0, 0.1],
-	fontSizes: [13, 16, 21, 26, 34, 48, 54, 63],
-	fontWeights: {
-		heading: "bold"
-	},
+	...type,
+	...components,
 	radii: [5, 8],
 	shadows: [
 		"0px 4px 4px rgba(0,0,0,.1)",
@@ -32,117 +26,6 @@ const theme = {
 		background: "white"
 	},
 	sizes: [200, 400, 600, 800, 1000, 1200],
-	layout: {
-		full: {
-			maxWidth: 5,
-			margin: `auto`
-		},
-		medium: {
-			width: 2,
-			padding: 2
-		},
-		small: {
-			width: 1,
-			padding: 1
-		}
-	},
-	buttons: {
-		primary: {
-			cursor: "pointer",
-			borderRadius: 0,
-			fontWeight: "bold",
-			color: "white",
-			background: "primary",
-			padding: props => `${props.space[1]}px ${props.space[2]}px`,
-			transition: props => `${props.transitions[0]}s`,
-			"&:hover": {
-				background: props => props.colors.grays[0],
-				transition: props => `${props.transitions[0]}s`
-			}
-		},
-		secondary: {
-			cursor: "pointer",
-			borderRadius: 0,
-			fontWeight: "bold",
-			color: "primary",
-			border: props => `1px solid ${props.colors.primary}`,
-			backgroundColor: "grays.7",
-			padding: props => `${props.space[1]}px ${props.space[2]}px`,
-			transition: props => `${props.transitions[0]}s`,
-			"&:hover": {
-				background: props => props.colors.primary,
-				color: "white",
-				transition: props => `${props.transitions[0]}s`
-			}
-		},
-		disabled: {
-			cursor: "pointer",
-			borderRadius: 0,
-			fontWeight: "bold",
-			color: "white",
-			backgroundColor: "grays.4",
-			padding: props => `${props.space[1]}px ${props.space[2]}px`
-		}
-	},
-	text: {
-		caps: {
-			textTransform: "uppercase",
-			letterSpacing: ".2em"
-		},
-		body: {
-			fontSize: [1]
-		},
-		display: {
-			letterSpacing: "-.01em",
-			fontWeight: "heading",
-			fontSize: 7
-		}
-	},
-	forms: {
-		medium: {
-			padding: 1
-		}
-	},
-	styles: {
-		root: {
-			width: `100%`,
-			fontFamily: "body",
-			margin: "auto",
-			color: "text",
-			backgroundColor: "grays.7",
-			li: {
-				listStyleType: "none"
-			}
-		},
-		h1: {
-			fontSize: 4
-		},
-		h2: {
-			fontSize: 3
-		},
-		h3: {
-			fontSize: 2
-		},
-		h4: {
-			fontSize: 1,
-			margin: 0
-		},
-		h5: {
-			fontSize: 0,
-			margin: 0,
-			padding: 0
-		},
-		p: {
-			fontSize: 1,
-			color: "grays.3",
-			lineHeight: 2
-		},
-		li: {
-			fontSize: 1,
-			color: "grays.3",
-			lineHeight: 2
-		}
-	},
 	transitions: [0.5]
 };
 
